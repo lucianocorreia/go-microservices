7179383e03651359f6f4be24aef06df7216dda22
Luciano Correia <luciano.correia@gmail.com>
2022-12-06 15:03:06 +0400

    initial commit

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	webport = "90"
+)
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	fmt.Printf("starting web broker on port %s\n", webport)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webport),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
